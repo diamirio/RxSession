2245db29a4cd544188a683dc028f07c49ab22cf7
Robin Mayerhofer <rm@tailored-apps.com>
2019-08-21 14:24:17 +0200

    [2] fix SwiftPM support

--- Package.swift
+++ Package.swift
@@ -1,6 +1,28 @@
+// swift-tools-version:5.0
+
 import PackageDescription
 
 let package = Package(
     name: "RxSession",
-    exclude: [ "RxSessionExample", "RxSessionTests" ]
+    products: [
+        .library(
+            name: "RxSession",
+            targets: ["RxSession"]),
+    ],
+    dependencies: [
+        .package(url: "git@github.com:tailoredmedia/Endpoints.git", .upToNextMajor(from: "0.5.0")),
+        .package(url: "git@github.com:ReactiveX/RxSwift.git", .upToNextMajor(from: "0.5.0"))
+    ],
+    targets: [
+        .target(
+            name: "RxSession",
+            dependencies: [],
+            path: "RxSession"
+        ),
+        .testTarget(
+            name: "RxSessionTests",
+            dependencies: ["RxSession"],
+            path: "RxSessionTests"
+        ),
+    ]
 )
